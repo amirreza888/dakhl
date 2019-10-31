8b80859e92944534be827fb88d3d10d645480c65
amir <amirfathi8888@gmail.com>
2019-10-31 20:09:42 +0330

    submit/expense and ....

--- web/models.py
+++ web/models.py
@@ -3,13 +3,22 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class Token(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+
+    def __str__(self):
+        return "{}_token".format(self.user)
+
+
+
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User,on_delete=models.CASCADE)
 
-    def __unicode__(self):
+    def __str__(self):
         return "{}-{}".format(self.date,self.amount)
 
 
@@ -21,5 +30,5 @@ class Income(models.Model):
     amount = models.BigIntegerField()
     user = models.ForeignKey(User,on_delete=models.CASCADE)
 
-    def __unicode__(self):
+    def __str__(self):
         return "{}-{}".format(self.date,self.amount)
