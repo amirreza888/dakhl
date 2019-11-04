84dc05a50ac45759a3eb7488d9d9d3aac1bfa123
amir <amirfathi8888@gmail.com>
2019-11-05 00:27:32 +0330

    url comes from and are not hardcoded

--- web/views.py
+++ web/views.py
@@ -71,7 +71,7 @@ def register(request):
                                  subject = "فعال سازی اکانت بستون",
                                  sender = "jadi@jadi.net",
                                  to = email,
-                                 text_body = "برای فعال سازی ایمیلی سیتون  خود روی لینک روبرو کلیک کنید: http://bestoon.ir/accounts/register/?email={}&code={}".format(email, code),
+                                 text_body = "http://bestoon.ir/accounts/register/?email={}&code={}".format(request.build_absolute_uri('/accounts/register/'), email, code),
                                  tag = "Create request")
                 message.send()
                 context = {'message': 'ایمیلی حاوی لینک فعال سازی اکانت به شما فرستاده شده، لطفا پس از چک کردن ایمیل، روی لینک کلیک کنید.'}
