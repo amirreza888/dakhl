13746de26aaaf16dcf7308b7f0fdc84c6f54dbb7
amir <amirfathi888@gmail.com>
2020-02-01 16:15:47 +0330

    oke

--- web/urls.py
+++ web/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('submit/expense/',views.submit_expense,name='submit_expense'),
     path('submit/income/',views.submit_income,name='submit_income'),
+    path('query/generalstat/',views.generalstat ,name='generalstat'),
     path('accounts/register/', views.register, name='register'),
     path('', views.index, name='index'),
 
